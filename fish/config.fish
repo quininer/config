b362a1c37e72d0c385a5a607cb4beaba141273c6
quininer kel <quininer@live.com>
2016-07-29 12:38:38 +0800

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,18 @@ set -x PATH /usr/bin /home/quininer/.cargo/bin /home/quininer/.local/bin
 set -gx SSH_ASKPASS (which ksshaskpass)
 set -gx EDITOR (which vi)
 sh ~/.login.sh
-fish_vi_mode
 
-# alias
+fish_vi_key_bindings
+fish_vi_cursor
+
+set -x CHROOT $HOME/.cache/chroot
+
+# alias ls
 function ls
 	exa $argv
+end
+
+# alias clip2vimcn
+function clip2vimcn
+	xclip -sel c -t image/png -o | curl -F "name=@-" "https://img.vim-cn.com/"
 end
