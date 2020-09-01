0542f26f078159f093536dd35ef786ac387c76cc
quininer <quininer@live.com>
2020-09-02 01:30:14 +0800

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,34 +1,25 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/home/quininer/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/quininer/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-set -x PATH /usr/bin /home/quininer/.cargo/bin /home/quininer/.local/bin /usr/lib/emscripten /usr/lib/emscripten-fastcomp
-
-# config
-set -gx SSH_ASKPASS (which ttyaskpass)
-set -gx EDITOR (which vi)
-sh ~/.login.sh
-
 fish_vi_key_bindings
 fish_vi_cursor
 
-set -x CHROOT $HOME/.cache/chroot
-set -x STARDICT_DATA_DIR $HOME/apps/dicts
-set -x LD_LIBRARY_PATH (rustc --print sysroot)/lib
-set -x RUST_SRC_PATH $LD_LIBRARY_PATH/rustlib/src/rust/src
-set -x CARGO_INCREMENTAL 1
+set -x PATH					$PATH $HOME/.local/bin $HOME/.cargo/bin
+set -x EDITOR				nvim
+set -x PAGER				less
+set -x LD_LIBRARY_PATH		(rustc --print sysroot)/lib
+set -x SSH_ASKPASS			(which ksshaskpass)
+set -x RUST_SRC_PATH		$LD_LIBRARY_PATH/rustlib/src/rust/src
+# set -x RUSTUP_DIST_SERVER	"https://mirrors.ustc.edu.cn/rust-static"
 
-# alias ls
-function ls
-	exa $argv
-end
+alias wget	"curl -O -C -"
+alias ls	exa
+alias grep	rg
+alias aa16	"aria2c -k1M -j16 -x16 -s16 --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "(chromium --version | string replace "Chromium " "Chrome/" | string trim -r --chars="Arch Linux")" Safari/537.36'"
 
-# alias clip2vimcn
-function clip2vimcn
-	xclip -sel c -t image/png -o | curl -F "name=@-" "https://img.vim-cn.com/"
-end
+# color man
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[01;32m")
+
+set -x CHROOT	$HOME/.cache/chroot
